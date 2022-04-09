34e6bcf4d760a1d95c7467c8f00c3593512b1c05
Haikal Saparudin <haikalst@users.noreply.github.com>
2022-04-09 18:16:15 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,52 @@
+const navItems = document.querySelector('#nav__items');
+const openNavBtn = document.querySelector('#open__nav-btn');
+const closeNavBtn = document.querySelector('#close__nav-btn');
+
+openNavBtn.addEventListener('click', () => {
+    navItems.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block';
+});
+
+const closeNav = () => {
+    navItems.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display = 'none';
+}
+
+closeNavBtn.addEventListener('click', closeNav);
+
+// Close nav menu when menu is clicked
+if (window.innerWidth < 1024) {
+    document.querySelectorAll('#nav__items li a').forEach(navItem => {
+        navItem.addEventListener('click', () => {
+            closeNav();
+        });
+    });
+}
+
+// Change navbar styles on scroll
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
+});
+
+// Swiper JS Testimonial
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    //Responsive Breakpoints
+    breakpoints: {
+        // When window width >= 600px
+        600: {
+            slidesPerView: 2
+        },
+        // When window width >= 1024px
+        1024: {
+            slidesPerView: 3
+        }
+    }
+});
